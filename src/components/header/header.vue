510feb96dc52cc37970984bf5615fe2e915c19a4
Kevin <kevinl@KL.local>
2019-03-03 14:28:26 +0800

    Feature: Header is done, now ready to build pop-up function

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -21,9 +21,14 @@
       <span class="count">{{seller.supports.length}}个</span>
       <i class="icon-keyboard_arrow_right"></i>
     </div>
-
   </div>
-  <div class="bulletin-wrapper"></div>
+  <div class="bulletin-wrapper">
+    <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+    <i class="icon-keyboard_arrow_right"></i>
+  </div>
+  <div class="background">
+    <img :src="seller.avatar" width="100%" height="100%">
+  </div>
 
 </div>
 </template>
@@ -47,15 +52,15 @@
 </script>
 
 <style scoped lang="stylus">
-@import '../../common/stylus/mixin.styl'
+@import '../../common/stylus/mixin'
 .header 
-  background: #999
+  position: relative
   color: #fff
+  background-color: rgba(7,17,27,0.5)
   .content-wrapper 
     position: relative
     padding: 24px 12px 18px 24px
     font-size: 0
-    
     .avatar 
       display: inline-block
       vertical-align: top
@@ -119,10 +124,45 @@
       background: rgba(0,0,0,0.2)
       text-align: center;
       .count
-        font-size: 10px
         vertical-align: top
+        font-size: 10px
       .icon-keyboard_arrow_right
         margin-left: 2px
         line-height: 24px
         font-size: 10px
+  .bulletin-wrapper
+    position: relative
+    height: 28px
+    line-height: 28px
+    padding: 0 22px 0 12px
+    white-space: nowrap
+    overflow: hidden
+    text-overflow: ellipsis
+    background: rgba(7,17,27,0.2)
+    .bulletin-title
+      display: inline-block
+      width: 22px
+      height: 12px
+      line-height: 28px
+      bg-image('bulletin')
+      background-size: 22px 12px
+      background-repeat: no-repeat
+      vertical-align: center;
+    .bulletin-text
+      vertical-align: top
+      margin: 0 4px
+      font-size: 10px
+    .icon-keyboard_arrow_right
+      position: absolute
+      font-size: 10px
+      right: 12px
+      top: 8px
+  .background
+    position: absolute
+    top: 0
+    left: 0
+    width: 100%
+    height: 100%
+    z-index: -1
+    filter: blur(10px)
 </style>
