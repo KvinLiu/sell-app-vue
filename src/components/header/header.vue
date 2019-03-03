4ed0d3ab7916b2fa290cc7fab6c5a91a9819e6d3
Kevin <kevinl@KL.local>
2019-03-03 15:25:14 +0800

    Feature: The Sticky footer layout is done

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,17 +17,33 @@
         <span class="text">{{seller.supports[0].description}}</span>
       </div>
     </div>
-    <div v-if="seller.supports" class="support-count">
+    <div v-if="seller.supports" class="support-count" @click="showDetail">
       <span class="count">{{seller.supports.length}}个</span>
       <i class="icon-keyboard_arrow_right"></i>
     </div>
   </div>
-  <div class="bulletin-wrapper">
+  <div class="bulletin-wrapper" @click="showDetail">
     <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
     <i class="icon-keyboard_arrow_right"></i>
   </div>
   <div class="background">
     <img :src="seller.avatar" width="100%" height="100%">
+  </div>
+  <div class="detail" v-show="detailShow">
+    <div class="detail-wrapper clearfix">
+      <div class="detail-main">
+        <p>{{seller.bulletin}}</p>
+        <p>{{seller.bulletin}}</p>
+        <p>{{seller.bulletin}}</p><p>{{seller.bulletin}}</p>
+        <p>{{seller.bulletin}}</p>
+        <p>{{seller.bulletin}}</p><p>{{seller.bulletin}}</p>
+        <p>{{seller.bulletin}}</p>
+        <p>{{seller.bulletin}}</p>
+      </div>
+    </div>
+    <div class="detail-close">
+      <i class="icon-close"></i>
+    </div>
   </div>
 
 </div>
@@ -38,6 +54,16 @@
     props: {
       seller: {
         type: Object
+      }
+    },
+    data() {
+      return {
+        detailShow: false
+      }
+    },
+    methods: {
+      showDetail() {
+        this.detailShow = true
       }
     },
     created() {
@@ -55,6 +81,7 @@
 @import '../../common/stylus/mixin'
 .header 
   position: relative
+  overflow: hidden
   color: #fff
   background-color: rgba(7,17,27,0.5)
   .content-wrapper 
@@ -165,4 +192,25 @@
     height: 100%
     z-index: -1
     filter: blur(10px)
+  .detail
+    position: fixed
+    z-index: 100
+    width: 100%
+    height: 100%
+    top: 0
+    left: 0
+    overflow: auto
+    background: rgba(7,17,27,0.8)
+    .detail-wrapper
+      min-height: 100%
+      .detail-main
+        margin-top: 64px
+        padding-bottom: 64px
+    .detail-close
+      position: relative
+      width: 32px
+      height: 32px
+      margin: -64px auto 0 auto
+      clear: both
+      font-size: 32px
 </style>
