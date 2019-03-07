2e431d1a439857ac07cf7dcfa90681f93dc49f81
Kevin <kevinl@KL.local>
2019-03-07 15:26:52 +0800

    feature: 购物车图标显示逻辑完成

---
+++ src/components/shopcart/shopcart.vue
@@ -0,0 +1,150 @@
+<template>
+  <div class="shopcart">
+    <div class="content">
+      <div class="content-left">
+        <div class="logo-wrapper">
+          <div class="logo" :class="{'highlight': totalCount>0}">
+            <i class="icon-shopping_cart" :class="{'highlight': totalCount>0}"></i>
+          </div>
+          <div class="num" v-show="totalCount">{{totalCount}}</div>
+        </div>
+        <div class="price" :class="{'highlight': totalPrice>0}">¥ {{totalPrice}}</div>
+        <div class="desc">配送费另需 ¥ {{deliveryPrice}}元</div>
+      </div>
+      <div class="content-right">
+        <div class="pay">
+          ¥ {{minPrice}}元起送
+        </div>
+      </div>
+    </div>    
+  </div>  
+</template>
+
+<script>
+export default {
+  props: {
+    selectFoods: {
+      type: Array,
+      default() {
+        return [{
+          price: 10,
+          count: 1
+        }]
+      }
+    },
+    deliveryPrice: {
+      type: Number,
+      default: 0
+    },
+    minPrice: {
+      type: Number,
+      default: 0
+    }
+  },
+  computed: {
+    totalPrice() {
+      let total = 0
+      this.selectFoods.forEach((food) => {
+        total += food.price * food.count
+      })
+      return total
+    },
+    totalCount() {
+      let count = 0
+      this.selectFoods.forEach(food => {
+        count += food.count
+      })
+      return count
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .shopcart
+    position: fixed
+    left: 0
+    bottom: 0
+    z-index: 50
+    width: 100%
+    height: 48px
+    .content
+      display: flex
+      height: 100%
+      background-color: #141d27
+      font-size: 0
+      .content-left
+        flex: 1
+        .logo-wrapper
+          display: inline-block
+          vertical-align: top
+          position: relative
+          top: -10px
+          margin: 0 12px
+          padding: 6px
+          box-sizing: border-box
+          width: 56px
+          height: 56px
+          background-color: #141d27
+          border-radius: 50%
+          text-align: center
+          .logo
+            width: 100%
+            height: 100%
+            border-radius: 50%
+            background-color: #2b343c
+            &.highlight
+              background-color: rgb(0, 160, 220)
+            .icon-shopping_cart
+              line-height: 44px
+              font-size: 24px
+              color: #80858a
+              &.highlight
+                color: #fff
+          .num
+            position: absolute
+            top: 0
+            right: 0
+            width: 24px
+            height: 16px  
+            line-height: 16px
+            text-align: center
+            border-radius: 16px
+            font-size:  9px
+            font-weight: 700
+            color: #ffffff
+            background: rgb(240, 20, 20)      
+            box-shadow: 0 4px 8px 0 rgba(0,0,0,0.4)
+        .price
+          display: inline-block
+          vertical-align: top
+          margin-top: 12px
+          line-height: 24px
+          padding-right: 12px
+          box-sizing: border-box
+          border-right: 1px solid rgba(255,255,255,0.1)
+          font-size: 16px
+          font-weight: 700
+          color: rgba(255, 255, 255, 0.4)
+          &.highlight
+            color: #fff
+        .desc
+          display: inline-block
+          vertical-align: top
+          line-height: 24px
+          margin: 12px 0 0 12px
+          color: rgba(255, 255, 255, 0.4)
+          font-size: 10px
+      .content-right
+        flex: 0 0 105px
+        width: 105px
+        background-color: #2b333b
+        .pay
+          height: 48px
+          line-height: 48px
+          text-align: center
+          font-size: 12px
+          color: rgba(255, 255, 255, 0.4)
+          font-weight: 700
+</style>
+
